1e1e7a8a22f60d3b70a565f272769d61b0446d29
Martin BESSEY <77329079+martinbessey@users.noreply.github.com>
2021-03-13 15:26:54 +0100

    Add files via upload

--- app-penduV2.js
+++ app-penduV2.js
@@ -1,6 +1,6 @@
 // Choisir un mot parmi une liste
 const arrayWords = ["Brick", "Construction", "Batman", "Amusing", "Imagination", "Legoman", "Danemark", "Legoland", "Building", "Duplo", "Emmet", "Spaceship"];
-let word = arrayWords[Math.floor(Math.random() * arrayWords.length)].toUpperCase();
+let word = arrayWords[Math.floor(Math.random() * arrayWords.length)].toLowerCase();
 
 
 // .map permet de recréer un tableau à partir d'un tableau
@@ -12,6 +12,7 @@ for (let i = 0; i < result.length; i++) {//On affiche autant de span que de lett
   document.querySelector('#result').innerHTML += `<span class="letter">${result[i]}</span>`;
 }
 
+let victory = false;
 
 let msg = document.getElementById("message");
 msg.innerHTML = "Select a letter";
@@ -22,7 +23,7 @@ msg.innerHTML = "Select a letter";
 }*/
 //Création d'une constante correspondant au boutons
 const touches = document.querySelectorAll("#keyboard button");
-
+victory
 for (let touche of touches){ 
   touche.addEventListener('click', function(){ 
     this.style.display = 'none';
@@ -30,9 +31,9 @@ for (let touche of touches){
     for (let i = 0; i < word.length; i++){
       if (this.innerHTML === word[i]){
         found = true;
-        result[i] = word[i];
+        result[i] === word[i];
         //console.log(result);
-        document.querySelector('#result').children[i].innerHTML = word[i];
+        document.querySelector("#result").children[i].innerHTML = word[i];
       }
     }
     if (!found){
@@ -40,9 +41,11 @@ for (let touche of touches){
       console.log('.hang_' + errors);
       document.querySelector('.hang_' + errors).style.opacity = 1;
     }
-    if (errors === 12){
+    if (errors === 7){
       msg.innerHTML = "GAME OVER!";
       document.querySelector('#keyboard').style.opacity = 0;
+      document.querySelector('#command').innerHTML += `<a href="index.html" class="key">Restart</button>`; 
     }
   });
+  
 }
